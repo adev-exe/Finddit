ad8ab444861a1b9d1d8c27ad9116ac095153691f
Me <calderona19@students.ecu.edu>
2021-10-06 17:14:57 -0400

    Added login page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect,url_for
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+
+@views.route('/login', methods=['GET', 'POST'])
+def index_func():
+    if request.method == 'POST':
+       
+        return redirect(url_for('index'))
+    return render_template('login.html')
