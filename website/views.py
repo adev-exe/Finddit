f378b18f6aa88797ce48e053c2bd17cfb5c26f18
Jahmad Attucks <attucksj15@students.ecu.edu>
2021-11-23 18:29:35 -0500

    Added database function to Signup/Login page and ability to create events

--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 import os
-from flask import Blueprint, render_template, request, redirect,url_for
+import functools
+from flask import (
+    Blueprint, render_template, request, redirect,url_for, session, flash, g
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from website.findditDB import get_db
 from werkzeug.utils import append_slash_redirect
 from .api import secrets
 from .api_utils import get_details, process_result
@@ -15,14 +21,104 @@ def home():
     # logo = os.path.join(app.config['UPLOAD_FOLDER'], 'Finddit.gif')
     return render_template("base.html", title = "Finddit")
 
-@views.route('/signuplogin.html')
+@views.route('/signuplogin.html', methods=('GET','POST'))
 def signuplogin():
-    return render_template("signuplogin.html")
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = request.form['password']
+        findditDB = get_db()
+        error = None
+        if not firstname:
+            error = 'First name required.'
+        elif not lastname:
+            error = 'Last name required.'
+        elif not email:
+            error = 'Email required.'
+        elif not password:
+            error = 'password name required.'
+        if error is None:
+            try:
+                findditDB.execute(
+                    "INSERT INTO user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)",
+                    (firstname, lastname, email, generate_password_hash(password)),
+                )
+                findditDB.commit()
+            except findditDB.IntegrityError:
+                error = f"Email {email} is already signed up"
+            else:
+                return redirect(url_for("views.signuplogin"))
+
+        flash(error)
+    #getting the login information entered into the login side of page
+    #and checking if the email and password are correct
+    if request.method == 'POST':
+        email2 = request.form['email']
+        password2 = request.form['password']
+        findditDB = get_db()
+        error = None
+        user = findditDB.execute( 'SELECT * FROM user WHERE email = ?', (email2,)).fetchone()
+        if user is None:
+            error = 'Email is wrong.'
+        elif not check_password_hash(user['password'], password2):
+            error = 'password is wrong.'
+        
+        if error is None:
+            session.clear()
+            session['user_first_name'] = user['firstname']
+            return redirect(url_for('base.html'))
+            
+        flash(error)
+
+    return render_template("views/signuplogin.html")
+
+@views.before_app_request
+def load_logged_in_user():
+    user_first_name = session.get('user_first_name')
+
+    if user_first_name is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_first_name,)
+        ).fetchone()
 
 @views.route('/events.html')
 def events():
     description = 'Description'
-    return render_template("events.html",  description = 'Description')
+    #get all events from the database and put them on the page
+    db = get_db()
+    posts = db.execute(
+        'SELECT e_id, event_name, event_date, event_time'
+        ' FROM event'
+    ).fetchall()
+    return render_template("events.html",  description = 'Description', posts=posts)
+#route for creating an event page
+@views.route('/create.html', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        event = request.form['eventN']
+        e_date = request.form['e_date']
+        e_time = request.form['e_time']
+        e_desc = request.form['e_desc']
+        error = None
+
+        if not event:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO event (event_name, event_date, event_time, event_desc)'
+                ' VALUES (?, ?, ?, ?)',
+                (event, e_date, e_time, e_desc)
+            )
+            db.commit()
+            return render_template('events.html')
+    return render_template('create.html')
 
 @views.route('/login', methods=['GET', 'POST'])
 def index_func():
